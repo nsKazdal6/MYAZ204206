5a5e07ae618f181762dea16b9a3b44edf4c189f9
Nazar Sıla Kazdal <nazarkazdal5191@gmail.com>
2023-06-09 17:03:25 +0300

    Hafta-7 Lab

---
+++ DataStructures/Queue/ArrayQueue.cs
@@ -0,0 +1,46 @@
+﻿using Queue.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class ArrayQueue<T> : IQueue<T>
+    {
+        private List<T> _innerArray;
+
+        public ArrayQueue()
+        {
+            _innerArray = new List<T>();
+        }
+        public ArrayQueue(IEnumerable<T> collection) :this()
+        {
+            foreach (var item in collection)
+            {
+                EnQueue(item);
+            } 
+        }
+        public int Count => _innerArray.Count;
+
+        public T DeQueue()
+        {
+            if (_innerArray.Count == 0)
+                throw new Exception("The queue is empty!");
+            var temp = _innerArray[0];
+            _innerArray.RemoveAt(0);
+            return temp;
+        }
+
+        public void EnQueue(T item)
+        {
+            _innerArray.Add(item);
+        }
+
+        public T Peek()
+        {
+            return Count == 0 ? default : _innerArray[0];
+        }
+    }
+}
---
+++ DataStructures/QueueTests/ArrayQueueTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTests
+{
+    public class ArrayQueueTests
+    {
+        [Fact]
+        public void ArrayQueue_Enqueue_Test()
+        {
+            var queue = new  Queue.ArrayQueue<char>("ahmet");
+
+            Assert.Equal(5, queue.Count);
+        }
+        [Fact]
+        public void ArrayQueue_Dequeue_Test()
+        {
+            var queue = new Queue.ArrayQueue<char>("ahmet");
+
+            var item = queue.DeQueue();
+
+            Assert.Equal(4, queue.Count);
+            Assert.Equal('a',item);
+        }
+        [Fact]
+        public void ArrayQueue_Dequeue_Exception_Test()
+        {
+            var queue = new Queue.ArrayQueue<char>();
+            Assert.Throws<Exception>(() => queue.DeQueue());
+        }
+        [Fact]
+        public void ArrayQueue_Peek_Test()
+        {
+            var queue = new Queue.ArrayQueue<char>("ahmet");
+
+            Assert.Equal('a',queue.Peek());
+        }
+    }
+}
