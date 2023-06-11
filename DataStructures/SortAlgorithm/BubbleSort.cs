597705e6dcd03d356a78db4e1acecef05b9c1380
Nazar Sıla Kazdal <nazarkazdal5191@gmail.com>
2023-06-12 00:53:22 +0300

    Hafta-9 Lab

---
+++ DataStructures/SortAlgorithm/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿namespace SortAlgorithm
+{
+    public class BubbleSort<T> where T : IComparable<T>
+    {
+        public static void Sort( T[] list)
+        {
+            bool isSorted = true;
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = 0; j < list.Length-i-1; j++)
+                {
+                    if (list[j].CompareTo(list[j + 1]) > 0)
+                    {
+                        Swap(ref list[j], ref list[j + 1]);
+                        isSorted = false;
+                    }
+                }
+                if (isSorted==true)
+                {
+                    break;
+                }
+            }
+            
+        }
+
+
+        private static void Swap(ref T x, ref T y)
+        {
+            var temp = x; 
+            x = y;
+            y = temp;
+        }
+    }
+}
